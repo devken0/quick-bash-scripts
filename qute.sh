dcb2a5e02174c307feec9fd9c6a1a687e8fb1055
Kozm <kennedyonipa@gmail.com>
2023-02-16 16:07:26 +0800

    Added gpu-accelerated qutebrowser shell script

--- qute.sh
+++ qute.sh
@@ -2,3 +2,5 @@
 
 # enable hardware acceleration
 qutebrowser --qt-flag ignore-gpu-blacklist --qt-flag enable-gpu-rasterization --qt-flag enable-native-gpu-memory-buffers --qt-flag num-raster-threads=4
+
+
