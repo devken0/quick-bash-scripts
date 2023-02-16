0e387de1f1c20e98a06a4edd2882e9a4f3771755
Kozm <kennedyonipa@gmail.com>
2023-02-16 15:28:43 +0800

    Added screencast using scrcpy shell script

---
+++ screencast.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# open a port
+# adb tcpip 5555
+
+# lower bitrate and resolution
+# scrcpy --bit-rate 2M --max-size 800
+
+# to uninstall sndcpy
+# adb uninstall com.rom1v.sndcpy
+
+# to enable usb again
+# adb usb
+
+# adb connect 192.168.1.2:5555
+adb devices
+scrcpy -m 1024
