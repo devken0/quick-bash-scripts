d1ad6b89b73edab5f5a3d6b441f2420bb83c5561
Kozm <kennedyonipa@gmail.com>
2023-02-16 15:20:09 +0800

    Added feh shell script

---
+++ fehbg.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#feh --no-fehbg --bg-center '/home/mc/Pictures/Facebook/wTami-50th-anniversary.png'
+feh --no-fehbg --bg-fill '/home/mc/Pictures/.wallpapers/anime-sky-lantern-mountain-japanese-castle-night-scenery-2k-wallpaper-5120x1440.jpg'
