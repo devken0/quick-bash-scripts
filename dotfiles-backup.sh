d1ab1220d6eca4cc60661239a377e83e5f6ff64b
Kennedy Onipa <kennedyonipa@gmail.com>
2023-02-20 17:56:37 +0800

    Updated script

--- dotfiles-backup.sh
+++ dotfiles-backup.sh
@@ -8,33 +8,33 @@
 #cp ~/.config/qutebrowser/quickmarks ~/Backups/dotfiles
 #cp ~/.config/qutebrowser/bookmarks/urls ~/Backups/dotfiles
 cp ~/.bashrc ~/Repos/dotfiles
-#cp ~/.vimrc ~/Backups/dotfiles
+#cp ~/.vimrc ~/Repos/dotfiles
 #cp ~/.gtkrc-2.0 ~/Backups/dotfiles
-#cp ~/.bash_history ~/Backups/dotfiles
+cp ~/.bash_history ~/Repos/dotfiles
 #cp ~/.config/picom.conf ~/Backups/dotfiles
 
 cd ~/Repos/dotfiles
 
-git add config
-git commit -m "my i3wm configuration file"
+#git add config
+#git commit -m "my i3wm configuration file"
 git add .bashrc
 git commit -m "my bashrc file"
-git add .vimrc
-git commit -m "my vimrc file"
-git add .gtkrc-2.0
-git commit -m "my themes configuration"
+#git add .vimrc
+#git commit -m "my vimrc file"
+#git add .gtkrc-2.0
+#git commit -m "my themes configuration"
 git add .bash_history
 git commit -m "my shell history"
-git add picom.conf
-git commit -m "my picom configuration"
-git add config.py
-git commit -m "my qutebrowser configuration"
-git add quickmarks
-git commit -m "my qutebrowser quickmarks"
-git add urls
-git commit -m "my qutebrowser bookmarks"
-git add mint-config
-git commit -m "linux mint i3 configuration"
+#git add picom.conf
+#git commit -m "my picom configuration"
+#git add config.py
+#git commit -m "my qutebrowser configuration"
+#git add quickmarks
+#git commit -m "my qutebrowser quickmarks"
+#git add urls
+#git commit -m "my qutebrowser bookmarks"
+#git add mint-config
+#git commit -m "linux mint i3 configuration"
 
 #git add *
 #git add -u
