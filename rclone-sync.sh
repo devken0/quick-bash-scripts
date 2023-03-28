099414ecff4723bbe71d125d1dfa93b78791a8aa
kozm-git <kennedyonipa@gmail.com>
2023-03-28 16:16:01 +0800

    Updated script

--- rclone-sync.sh
+++ rclone-sync.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
 #rclone sync -P ~/Archive/ tup:
-rclone sync ~/Archive od-tup:PC\ Backup/
+rclone sync ~/Archive od-tup:PC\ Backup/Archive/
+rclone sync ~/Downloads od-tup:PC\ Backup/Downloads/
+rclone sync ~/Music od-tup:PC\ Backup/Music/
+rclone sync ~/Pictures od-tup:PC\ Backup/Pictures/
+rclone sync ~/Programming od-tup:PC\ Backup/Programming/
+rclone sync ~/Videos od-tup:PC\ Backup/Programming/
