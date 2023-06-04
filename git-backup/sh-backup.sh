30cd9e4848076d204daa15d64174efa2e282f133
kozm-git <kennedyonipa@gmail.com>
2023-06-05 03:44:01 +0800

    Added script

---
+++ git-backup/sh-backup.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#
+#-------------------------Go to Repository 
+cd ~/Repos/sh
+#-------------------------Credentials
+#git config user.name "Kozm"
+#git config user.email "kennedyonipa@gmail.com"
+
+eval "$(ssh-agent -s)"
+
+ssh-add ~/.ssh/github_rsa
+
+#-------------------------Stage and Commit
+git add -u
+git commit -m "Updated script" 
+
+git add *
+git commit -m "Added script"
+
+#------------------------Pull and Push Changes
+git pull
+git push 
+
+#------------------------END
+#echo "Done."
+#echo "Press Enter"
+#read
