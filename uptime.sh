021c39c42f228e06c9c5906b82ca28f623441312
kozm-git <kennedyonipa@gmail.com>
2023-06-20 20:01:01 +0800

    Updated script

--- uptime.sh
+++ uptime.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
-echo "$(uptime -p)" > ~/.config/i3/uptime
+#echo "$(uptime -p)" > ~/.config/i3/uptime
+
+uptime=$(uptime -p)
+days=$(echo "$uptime" | grep -oP '\d+(?= day)')
+hours=$(echo "$uptime" | grep -oP '\d+(?= hour)')
+minutes=$(echo "$uptime" | grep -oP '\d+(?= minute)')
+
+formatted_uptime=""
+if [ -n "$days" ]; then
+  formatted_uptime="${days}d "
+fi
+formatted_uptime="${formatted_uptime}${hours}h ${minutes}m"
+
+echo "$formatted_uptime" > ~/.config/i3/uptime
