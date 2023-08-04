535e634e0fda09ba0a74be04e64de91376500b13
kozm-git <kennedyonipa@gmail.com>
2023-08-04 13:01:01 +0800

    Added script

---
+++ git-backup/py-backup.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+cd ~/Repos/asm
+
+#-------------------------Create New Repository
+#
+#git init
+#git branch -M main
+#git remote add origin https://github.com/kozm-git/py.git
+
+#-------------------------Credentials
+#
+#git config user.name "Kozm"
+#git config user.email "kennedyonipa@gmail.com"
+
+eval "$(ssh-agent -s)"
+
+ssh-add ~/.ssh/github_rsa
+
+#-------------------------Stage and Commit
+
+git add -u
+git commit -m "Updated assembly file" 
+
+git add *
+git commit -m "Added assembly file"
+
+#------------------------Pull and Push Changes
+
+git pull 
+#git push
+git push 
+
+#------------------------END
+#echo "Done."
+#echo "Press Enter"
+#read
