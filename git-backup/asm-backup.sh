164eda0d2319b42ede87d3e71fe38fa6aa097077
kozm-git <kennedyonipa@gmail.com>
2023-08-04 12:21:01 +0800

    Updated script

--- git-backup/asm-backup.sh
+++ git-backup/asm-backup.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 #
 #-------------------------Go to Repository 
-cd ~/Repos/assembly
+
+cd ~/Repos/asm
+
 #-------------------------Credentials
+#
 #git config user.name "Kozm"
 #git config user.email "kennedyonipa@gmail.com"
 
@@ -11,6 +14,7 @@ eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/github_rsa
 
 #-------------------------Stage and Commit
+
 git add -u
 git commit -m "Updated assembly file" 
 
@@ -18,6 +22,7 @@ git add *
 git commit -m "Added assembly file"
 
 #------------------------Pull and Push Changes
+
 git pull
 git push
 
